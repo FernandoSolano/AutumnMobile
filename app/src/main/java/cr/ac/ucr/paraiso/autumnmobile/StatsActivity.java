2bece7534d8e250eac508e1a7138c26d965de9eb
Fernando Solano <fer_odi10@hotmail.com>
2018-06-19 10:44:23 -0600

    Charts integration and samples

--- app/src/main/java/cr/ac/ucr/paraiso/autumnmobile/StatsActivity.java
+++ app/src/main/java/cr/ac/ucr/paraiso/autumnmobile/StatsActivity.java
@@ -6,6 +6,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+
 public class StatsActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +25,46 @@ public class StatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
         setTitle("Estadísticas");
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 7),
+                new DataPoint(4, 15),
+                new DataPoint(5, 10)
+        });
+        graph.addSeries(series);
+
+        PieChart pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(8f, 0));
+        yvalues.add(new Entry(15f, 1));
+        yvalues.add(new Entry(12f, 2));
+        yvalues.add(new Entry(25f, 3));
+        yvalues.add(new Entry(23f, 4));
+        yvalues.add(new Entry(17f, 5));
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
+        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+        xVals.add("June");
+        PieData data = new PieData(xVals, dataSet);
+        // In percentage Term
+        data.setValueFormatter(new PercentFormatter());
+        // Default value
+        //data.setValueFormatter(new DefaultValueFormatter(0));
+        pieChart.setData(data);
+        pieChart.setDescription("This is Pie Chart");
     }
 
     @Override
